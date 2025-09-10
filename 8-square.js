54508ca4bdbc1e68ad9bb7451ac113ab3d5beca0
akrate <oussamaakrate590@gmail.com>
2025-09-10 19:53:28 +0100

    square

--- 8-square.js
+++ 8-square.js
@@ -1,16 +1,15 @@
 const arg = process.argv.slice(2);
-if (!isNaN(arg[0]))
-{
-    num = parseInt(arg[0],10);
-    if (num > 0)
-    {
-        i = 0;
-        while (i < num)
-        {
-            console.log('x'.repeat(num));
+const input = arg[0];
+
+if (!isNaN(input)) {
+    const num = parseInt(input, 10);
+    if (num > 0) {
+        let i = 0;
+        while (i < num) {
+            console.log('X'.repeat(num));
             i++;
         }
     }
-}else{
-    console.log("Missing size");
+} else {
+    console.log('Missing size');
 }
